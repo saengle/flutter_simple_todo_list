dd683f97dfe652817e8cbd1b4aef516dc6879fc6
saengle <zohv817@gmail.com>
2023-01-31 18:24:38 +0900

    Start TodoList Project

---
+++ android/app/src/main/kotlin/com/saengle/todolist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.saengle.todolist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListScreen extends StatelessWidget {
+  const ListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo 리스트'),
+      ),
+      body: Container(),
+    );
+  }
+}
