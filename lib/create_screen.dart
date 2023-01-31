f4601bfca1d706a3045af6c98d0aad9e3f651131
saengle <zohv817@gmail.com>
2023-01-31 18:39:21 +0900

    Add: Create Screen 기본 형태

---
+++ lib/create_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CreateScreen extends StatelessWidget {
+  const CreateScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo 작성'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.done),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              hintText: '할일을 입력하세요',
+              hintStyle: const TextStyle(color: Colors.blueGrey)),
+        ),
+      ),
+    );
+  }
+}
